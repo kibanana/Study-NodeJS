7888468dfff5a84df6dcda6392aefc4c9f8d9d6e
kyw017763 <kyw017763@gmail.com>
2019-05-16 01:20:57 +0900

    ~036 (반복문)

---
+++ ex007.js
@@ -0,0 +1,58 @@
+//031,032 - for문 (중첩 - 구구단)
+for(let i=2; i<10; i++) {
+    console.log(`===== ${i} 단 출력 =====`);
+    for(let j=1; j<10; j++) {
+        console.log(`${i} * ${j} = ${i*j}`);
+    }
+    console.log(`===== 출력 끝 =====`);
+}
+
+console.log();
+
+//033 - for문-break
+const fruitList = [
+    {name: 'grape', level: 3},
+    {name: 'lemon', level: 1},
+    {name: 'apple', level: 5},
+    {name: 'melon', level: 7},
+    {name: 'watermelon', level: 6},
+    {name: 'orange', level: 3}
+];
+
+let fruitResult = '';
+
+for(let i=0; i<fruitList.length; i++) {
+    if(fruitList[i].name === 'melon') {
+        fruitResult = fruitList[i];
+        break;
+    }
+    console.log(`${fruitList[i].name} 은 melon이 아닙니다`);
+}
+console.log(fruitResult);
+
+console.log();
+
+//034 - for of
+for(const fruit of fruitList) {
+    console.log(fruit);
+}
+
+console.log();
+
+//035 - forEach
+fruitList.forEach(function(fruit){
+    console.log(fruit)
+});
+console.log();
+fruitList.forEach(fruit => console.log(fruit));
+
+console.log();
+
+//036 - while
+let i=0;
+while(i<10) {
+    console.log(i);
+    i++;
+}
+
+console.log();
