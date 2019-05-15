4b279dce863fceffb8e4b20a80af3a9d5ee721c6
kyw017763 <kyw017763@gmail.com>
2019-05-16 02:56:14 +0900

    ~42 (try-catch)

---
+++ ex009.js
@@ -0,0 +1,30 @@
+//041
+try {
+    printMessage('hello');
+} catch (e) {
+    console.log('error: ', e);
+}
+console.log('program finished');
+
+console.log();
+
+//042
+const printHello = () => console.log('hello');
+
+try {
+    printHello();
+} catch(e) {
+    console.error(e);
+} finally {
+    console.log('finally 1');
+}
+
+try {
+    printBye();
+} catch(e) {
+    console.error(e);
+} finally {
+    console.log('finally 2');
+}
+
+console.log();
