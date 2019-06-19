782d90c539ab764cbd101857f4a0dd6d5858624f
kyw017763 <kyw017763@gmail.com>
2019-06-20 01:09:10 +0900

    Express ~ HTML

---
+++ server_test04.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'html')));
+
+// app.use('/static', express.static(path.join(__dirname, 'html')));
+// 모든 접근에 /static 이 포함되어야 함
+// ex ) http://localhost:3000/static/test02_main.html
+// 하지만 이 방법 안썼으니까 아래(get, sendFile)에서는 안쓰고 파일이름만 써서 접근할 수 있음
+
+//html은 폴더명
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'test02_main.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'html', 'test02_about.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Express App on port 3000!');
+});
