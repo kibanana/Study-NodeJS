8dcf52675b83e1b19afafc13d05e74406689f361
kyw017763 <kyw017763@gmail.com>
2019-08-24 19:14:07 +0900

    네이버 급상승 검색어 crawling

---
+++ crawler_keywords.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const log = console.log;
+
+const getHtml = async () => {
+  try {
+    return await axios.get("https://www.naver.com/");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml()
+  .then(html => {
+    let ulList = [];
+    const $ = cheerio.load(html.data);
+    const $bodyList = $("div.ah_roll_area ul.ah_l").children("li.ah_item");
+
+    $bodyList.each(function(i, elem) {
+      ulList[i] = {
+          rank : $(this).find('span.ah_r').text(),
+          keyword : $(this).find('span.ah_k').text()
+      };
+      
+
+
+      console.log($(this).find('span.ah_r').text());
+    });
+
+    const data = ulList.filter(n => n.rank);
+    return data;
+    
+  })
+  .then(res => log(res));
