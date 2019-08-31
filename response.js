e549fd37a06cdd62a956926c792fbf87b6236865
kyw017763 <kyw017763@gmail.com>
2019-08-31 23:45:09 +0900

    response

---
+++ response.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+
+app.get('/sendfile', (req, res) => {
+    res.sendFile(path.join(__dirname,'package.json'));
+});
+
+app.get('/json', (req, res) => {
+    fs.readFile(path.join(__dirname,'package.json'), (err, data) => {
+        if(data) {
+            console.log(data);
+            res.json(JSON.stringify(data.toString()));
+        } else {
+            const jsonError = {
+                "error" : "Can't read pacage.json"
+            };
+            res.json(jsonError);
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is working');
+});
