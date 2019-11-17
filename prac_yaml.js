773c58600979af26fcc399e24e1b1dfa9dfd2268
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2019-11-18 03:07:45 +0900

    Delete yamljs (use js-yaml instead this)

--- prac_yaml.js
+++ prac_yaml.js
@@ -1,14 +1,6 @@
-// import YAML from 'yamljs';
 import JSYAML from 'js-yaml';
 import fs from 'fs';
 import path from 'path';
-
-// let native;
-// YAML.load(path.join(__dirname, 'pracYaml.yml'), (res) => {
-//   native = res;
-// });
-// const parsedObject = YAML.parse(native);
-// const yamlString = YAML.stringify(parsedObject, 4);
 
 // fs.readFileSync 가 아닌, fs.readFile 사용 시 undefined 반환
 const pracNative = JSYAML.safeLoadAll(fs.readFileSync(path.join(__dirname, 'pracYaml.yml'), 'utf8', (err, data) => data));
