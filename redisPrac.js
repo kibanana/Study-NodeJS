cccc99880d469d7fcb5c8cbb692e5bc806903da3
Yewon Kim <37951612+kyw017763@users.noreply.github.com>
2019-11-27 02:15:45 +0900

    Practice redis

--- redisPrac.js
+++ redisPrac.js
@@ -26,8 +26,36 @@ const redisClient = redis.createClient();
 //   console.log(JSON.stringify(res));
 // }); // ["Thunderclouds", "Carnival", "Smile", "Reminder"]
 
-// set (key-set) 배열과 비슷하지만 중복 데이터 허용하지 않음
-redisClient.sadd('song', 'Smile', 'Reminder'); // , 'Reminder');
-redisClient.smembers('song', (err, res) => {
-  console.log(res);
-});
+// set (key-set) 배열과 비슷하지만 중복 데이터 허용하지 않음(만약 넣는다면 무시됨)
+
+// redisClient.sadd('song', 'Smile', 'Reminder', 'Reminder');
+// redisClient.smembers('song', (err, res) => {
+//   console.log(res);
+// });
+
+// sorted set (key-sorted set)
+// list와 비슷한 방법으로 데이터를 가져온다
+// 기본 오름차순 정렬
+
+// redisClient.ZADD('score', 100, 'A', 80, 'B', 60, 'C', 40, 'D');
+// redisClient.ZREVRANGE('score', 0, -1, (err, res) => {
+//   console.log(res); // ["A", "B", "C", "D"]
+// });
+// redisClient.ZRANGE('score', 0, -1, (err, res) => {
+//   console.log(res); // ["D", "C", "B", "A"]
+// });
+
+// del
+// redisClient.del('name');
+// redisClient.get('name', (err, res) => {
+//   console.log(res);
+// });
+
+// exists
+// console.log(redisClient.exists('name'));
+
+// rename
+// redisClient.rename('name', 'myName');
+// redisClient.get('myName', (err, res) => {
+//   console.log(res);
+// });
